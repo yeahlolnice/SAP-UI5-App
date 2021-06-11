b27c9b3aebf87ca5a3fe86257459375d6e55028d
yeahlolnice <mattwilly98@hotmail.com>
2021-06-11 21:27:13 +1000

    added controller and message toast

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.walkthrough", {
+        onShowHello : function () {
+            // show js alert
+            MessageToast.show("hello there!")
+        }
+    });
+})
