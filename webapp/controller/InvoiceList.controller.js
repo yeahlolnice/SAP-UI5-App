1bc8dd888e9679c68b95c9ec496ddf7f70384e94
yeahlolnice <mattwilly98@hotmail.com>
2021-06-15 19:01:38 +1000

    added custom formatter

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,16 +1,18 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+    "../model/formatter"
+], function (Controller, JSONModel, formatter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
-
+        formatter: formatter,
 		onInit : function () {
 			var oViewModel = new JSONModel({
 				currency: "AUD"
 			});
 			this.getView().setModel(oViewModel, "view");
+            
 		}
 	});
 });
